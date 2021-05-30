8108605786977d282ed8d3ef5eecefd459ac860a
sirafr <andres22f@hotmail.com>
2021-05-30 15:23:38 -0500

    finalVer

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,6 +1,14 @@
 //const store = require('../../../store/mysql');
 
-const store = require('../../../store/remote-mysql')
+const config = require("../../../config");
+
+let store;
+if (config.remoteDB === true) {
+    store = require('../../../store/remote-mysql')
+} else {
+    store = require("../../../store/mysql");
+}
+//const store = require('../../../store/remote-mysql')
 const ctrl = require('./controller');
 
 module.exports = ctrl(store);
